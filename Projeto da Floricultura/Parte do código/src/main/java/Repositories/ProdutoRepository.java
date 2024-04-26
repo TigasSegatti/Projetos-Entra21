299a77ad77d7737d665c768496182e2fda90a82a
Tiago Segatti <tiago01segatti@gmail.com>
2024-04-26 10:04:58 -0300

    Organizando

--- Projeto da Floricultura/Parte do código/src/main/java/Repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Models.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {	
-
-}
