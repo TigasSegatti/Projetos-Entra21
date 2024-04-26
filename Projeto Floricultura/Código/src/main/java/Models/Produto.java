7f258cd73c2586acb828c7a9097b00541a93eee8
Tiago Segatti <tiago01segatti@gmail.com>
2024-04-26 10:09:29 -0300

    Adicionando código

---
+++ Projeto Floricultura/Código/src/main/java/Models/Produto.java
@@ -0,0 +1,61 @@
+package Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter 
+@Setter 
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+	public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idProduto;
+	private String nome;
+	private String tipo;
+	private Double preco;
+	private Integer quantidadeEstoque;
+	
+	public 
+	public Long getIdProduto() {
+		return idProduto;
+	}
+	public Produto setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+
+}
